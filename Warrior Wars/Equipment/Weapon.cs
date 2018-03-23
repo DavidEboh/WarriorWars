c8cfb6618a23f1b25f49bb77ade2d32d43169864
Deboh <david.ebohttu@gmail.com>
2018-03-23 18:27:54 -0500

    first commit

---
+++ Warrior Wars/Equipment/Weapon.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using WarriorWars.Enum;
+
+namespace WarriorWars.Equipment
+{
+    class Weapon
+    {
+
+        private int GOOD_GUY_DAMAGE = 10;
+        private int BAD_GUY_DAMAGE = 10;
+
+        private int damage;
+
+        public int Damage
+        {
+            get
+            {
+                return damage;
+            }
+        }
+
+        public Weapon(Faction faction)
+        {
+            switch (faction)
+            {
+                case Faction.GoodGuy:
+                    damage = GOOD_GUY_DAMAGE;
+                    break;
+                case Faction.BadGuy:
+                    damage = BAD_GUY_DAMAGE;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+    }
+}
---
+++ Warrior Wars/Warrior.cs
@@ -0,0 +1,77 @@
+﻿using WarriorWars.Enum;
+using WarriorWars.Equipment;
+using System;
+
+namespace WarriorWars
+{
+    class Warrior
+    {
+        private const int GOOD_GUY_STARTING_HEALTH = 50;
+        private const int BAD_GUY_STARTING_HEALTH = 50;
+
+        private readonly Faction FACTION;
+
+        private int health;
+        private string name;
+        private bool isAlive;
+
+        public bool IsAlive
+        {
+            get
+            {
+                return isAlive;
+            }
+        }
+
+        private Weapon weapon;
+        private Armor armor;
+
+        public Warrior(string name, Faction faction)
+        {
+            this.name = name;
+            FACTION = faction;
+            isAlive = true;
+
+            switch (faction)
+            {
+                case Faction.GoodGuy:
+                    weapon = new Weapon(faction);
+                    armor = new Armor(faction);
+                    health = GOOD_GUY_STARTING_HEALTH;
+
+                    break;
+                case Faction.BadGuy:
+                    weapon = new Weapon(faction);
+                    armor = new Armor(faction);
+                    health = GOOD_GUY_STARTING_HEALTH;
+
+                    break;
+                default:
+                    break;
+            }
+
+        }
+
+        public void Attack(Warrior enemy)
+        {
+            int damage = weapon.Damage / enemy.armor.ArmorPoints;
+
+            enemy.health = enemy.health - damage;
+
+            if (enemy.health <= 0)
+            {
+                enemy.isAlive = false;
+                Tools.ColorfulWriteLine($"{enemy.name} died a warriors death!", ConsoleColor.Red);
+                Tools.ColorfulWriteLine($"{ name} is the victor", ConsoleColor.Green);
+            }
+            else
+            {
+                Console.WriteLine($"{name} attacked {enemy.name} for {damage}. \n" +
+                    $"{name} current health: {health}\n" +
+                    $"{enemy.name} current health: {health}\n" +
+                    new string('-', 40) + "\n"); 
+
+            }
+        }
+    }
+}
